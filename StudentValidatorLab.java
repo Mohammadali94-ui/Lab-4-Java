f379c445e9670854407da822256f9c1c66e67eb7
Mohammadali94-ui <200611711@student.georgianc.on.ca>
2026-03-09 09:11:38 -0400

    Initial commit: added lab shell and name comment

---
+++ StudentValidatorLab.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.util.regex.*;
+
+// Mohammad Ali Bokhari
+// Lab 4 - Java Regex Validator
+
+public class StudentValidatorLab {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.close();
+    }
+}
